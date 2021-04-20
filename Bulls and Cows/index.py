ade93588bdea960add24918078d3bfa87b855e24
Andrea Georgieva <andrea.georgieva@outlook.com>
2021-04-20 15:10:55 +0300

    General changes made

--- Bulls and Cows/index.py
+++ Bulls and Cows/index.py
@@ -5,22 +5,20 @@ from tkinter import *
 class CowsAndBulls:
     def __init__(self):
         self.code = []
-        self.window = None
+        self.window = Tk()
+        self.frame = Frame(self.window)
+        self.width = 300
         self.height = 250
+        self.plus_height = 0
         self.guesses = 10
-        self.label_y = 225
-        self.input_label = None
         self.input = None
-        self.submit_btn = None
-
-    @classmethod
-    def new_game(cls):
-        return cls()
 
     def setup(self):
-        self.window = Tk()
         self.window.title('Bulls & Cows')
-        self.window.geometry(f'300x{self.height}')
+        width = (self.window.winfo_screenwidth() - self.width) / 2
+        height = (self.window.winfo_screenheight() - self.height) / 2
+        self.window.geometry(f'{self.width}x{self.height}+{int(width)}+{int(height)}')
+
         start_button = Button(self.window, text='START GAME', font='Calibri',
                               command=lambda: self.start_game_btn(start_button))
         start_button.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -44,27 +42,27 @@ class CowsAndBulls:
         return False
 
     def start_game_btn(self, start_button):
-        label = Label(self.window, text='ENTER GUESS', font='Calibri')
-        self.input_label = label
-        label.place(x=150, y=75, anchor=CENTER)
-        input_for_label = Entry(self.window, font='Calibri')
-        self.input = input_for_label
-        input_for_label.place(x=150, y=125, anchor=CENTER)
-        submit_guess_btn = Button(self.window, text='SUBMIT', font='Calibri', command=self.play)
-        self.submit_btn = submit_guess_btn
-        submit_guess_btn.place(x=150, y=175, anchor=CENTER)
+        # self.frame.grid(row=0, column=0)
+        self.frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+        label = Label(self.frame, text='ENTER GUESS', font='Calibri', width=15).grid(row=0, column=0, pady=10, padx=10)
+        self.input = Entry(self.frame, font='Calibri', width=15)
+        self.input.grid(row=1, column=0, pady=10, padx=10)
+        submit_guess_btn = Button(self.frame, text='SUBMIT', font='Calibri', width=10, command=self.play).grid(row=2, column=0, pady=10, padx=10)
         start_button.destroy()
+        # self.frame.update()
+        # print(self.frame.winfo_width())
+        # print(self.frame.winfo_height())
         # code_label = Label(self.window, text=self.code, font='Calibri')
         # code_label.place(x=150, rely=0.05, anchor=CENTER)
-        # code_label.place_forget()
 
     def play(self):
         if not self.guesses:
-            self.destroy_input()
-            message_label = Label(self.window,
+            [child.destroy() for child in self.frame.winfo_children()]
+            self.window.geometry(f'{self.width}x{self.height}')
+            message_label = Label(self.frame,
                                   text=f'You ran out of tries.\nThe correct code is {"".join(self.code)}',
                                   font='Calibri')
-            message_label.place(x=150, y=100, anchor=CENTER)
+            message_label.place(relx=0.5, rely=0.4, anchor=CENTER)
             self.start_new_game()
         else:
             self.guesses -= 1
@@ -73,8 +71,8 @@ class CowsAndBulls:
             cows = 0
 
             if guess == self.code:
-                self.destroy_input()
-                message_label = Label(self.window,
+                [child.destroy() for child in self.frame.winfo_children()]
+                message_label = Label(self.frame,
                                       text=f'Correct! The code is {"".join(self.code)}',
                                       font='Calibri')
                 message_label.place(x=150, y=100, anchor=CENTER)
@@ -87,23 +85,19 @@ class CowsAndBulls:
                     elif digit in self.code:
                         cows += 1
 
-                message_label = Label(self.window, text=f'Guess: {"".join(guess)} (Bulls: {bulls}, Cows: {cows})',
+                message_label = Label(self.frame, text=f'Guess: {"".join(guess)} (Bulls: {bulls}, Cows: {cows})',
                                       font='Calibri')
-                message_label.place(x=150, y=self.label_y, anchor=CENTER)
-                self.label_y += 20
-                self.height += 20
-                self.window.geometry(f'300x{self.height}')
-
-    def destroy_input(self):
-        self.input_label.destroy()
-        self.input.destroy()
-        self.submit_btn.destroy()
+                # message_label.place(x=150, y=self.label_y, anchor=CENTER)
+                message_label.grid()
+                self.plus_height += 20
+                self.window.geometry(f'300x{self.height + self.plus_height}')
 
     def start_new_game(self):
-        retry_btn = Button(self.window, text='RETRY', font='Calibri', command=self.new_game_setup)
-        retry_btn.place(x=150, y=150, anchor=CENTER)
+        retry_btn = Button(self.frame, text='RETRY', font='Calibri', command=self.new_game_setup)
+        retry_btn.place(relx=0.5, rely=0.65, anchor=CENTER)
 
     def new_game_setup(self):
-        self.window.destroy()
-        game = self.new_game()
-        game.setup()
+        [child.destroy() for child in self.frame.winfo_children()]
+        self.guesses = 10
+        self.plus_height = 0
+        self.setup()
