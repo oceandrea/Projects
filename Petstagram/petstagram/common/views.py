a826422dfbf5bed46f4979d1e35205834389b790
Andrea Georgieva <andrea.georgieva@outlook.com>
2021-06-22 01:31:11 +0300

    Home view to CBV

--- Petstagram/petstagram/common/views.py
+++ Petstagram/petstagram/common/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def landing_page(request):
-    context = {
-        'title': 'Petstagram',
-    }
-    return render(request, 'landing_page.html', context)
+# def landing_page(request):
+#     context = {
+#         'title': 'Petstagram',
+#     }
+#     return render(request, 'landing_page.html', context)
+
+
+class HomeView(TemplateView):
+    template_name = 'landing_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Petstagram'
+        return context
