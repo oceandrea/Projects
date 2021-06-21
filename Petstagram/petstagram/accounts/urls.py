c047e9bb857aa2102edbd9aa14f6c0f671d357ec
Andrea Georgieva <andrea.georgieva@outlook.com>
2021-06-22 01:20:25 +0300

    Profile view to CBV

--- Petstagram/petstagram/accounts/urls.py
+++ Petstagram/petstagram/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
-from petstagram.accounts.views import show_profile, RegisterView
+from petstagram.accounts.views import RegisterView, ProfileView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('profile/', show_profile, name='user profile'),
-    path('profile/<int:pk>/', show_profile, name='user profile'),
+    # path('profile/', show_profile, name='user profile'),
+    # path('profile/<int:pk>/', show_profile, name='user profile'),
+    path('profile/', ProfileView.as_view(), name='user profile'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='user profile'),
     # path('register/', register, name='register'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- Petstagram/petstagram/accounts/views.py
+++ Petstagram/petstagram/accounts/views.py
@@ -6,34 +6,55 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from petstagram.accounts.forms import ProfileForm
 from petstagram.accounts.models import UserProfile
 from petstagram.pets.models import Pet
 
 
-@login_required
-def show_profile(request, pk=None):
-    user = request.user if pk is None else User.objects.get(id=pk)
-    own_profile = user == request.user
-    if request.method == 'POST':
-        old_photo = user.userprofile.profile_picture
-        form = ProfileForm(request.POST, request.FILES, instance=user.userprofile)
-        if form.is_valid():
-            if request.FILES:
-                os.remove(old_photo.path)
-            form.save()
-            redirect('user profile')
+# @login_required
+# def show_profile(request, pk=None):
+#     user = request.user if pk is None else User.objects.get(id=pk)
+#     own_profile = user == request.user
+#     if request.method == 'POST':
+#         old_photo = user.userprofile.profile_picture
+#         form = ProfileForm(request.POST, request.FILES, instance=user.userprofile)
+#         if form.is_valid():
+#             if request.FILES:
+#                 os.remove(old_photo.path)
+#             form.save()
+#             redirect('user profile')
+#
+#     context = {
+#         'own_profile': own_profile,
+#         'profile_user': user,
+#         'form': ProfileForm(),
+#         'pets': [pet for pet in Pet.objects.all() if pet.user.user == user],
+#     }
+#     return render(request, 'registration/profile.html', context)
 
-    context = {
-        'own_profile': own_profile,
-        'profile_user': user,
-        'form': ProfileForm(),
-        'pets': [pet for pet in Pet.objects.all() if pet.user.user == user],
-    }
-    return render(request, 'registration/profile.html', context)
 
+class ProfileView(UpdateView):
+    template_name = 'registration/profile.html'
+    form_class = ProfileForm
+    model = UserProfile
+    # context_object_name = 'profile_user'
+    success_url = reverse_lazy('user profile')
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk', None)
+        user = self.request.user if pk is None else User.objects.get(id=pk)
+
+        return user.userprofile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_user'] = self.get_object().user
+        context['own_profile'] = self.get_object().user == self.request.user
+        context['pets'] = [pet for pet in Pet.objects.all() if pet.user.user == self.get_object().user]
+
+        return context
 
 # def register(request):
 #     form = UserCreationForm()
